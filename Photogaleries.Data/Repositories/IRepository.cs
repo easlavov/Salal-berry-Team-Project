aec7a15d294dd980e5bf5866357dda377a11deed
Stoyan Stoyanov <sss17@mail.com>
2014-09-17 17:48:02 +0300

    Added Ninject dependancy resolver and PhotosController class...

--- Photogaleries.Data/Repositories/IRepository.cs
+++ Photogaleries.Data/Repositories/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,16 @@ namespace Photogaleries.Data.Repositories
 {
     public interface IRepository<T>
     {
+        IQueryable<T> All();
+
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> conditions);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Detach(T entity);
     }
 }
--- Photogaleries.Services/Startup.cs
+++ Photogaleries.Services/Startup.cs
@@ -1,18 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Reflection;
+using System.Web.Http;
 using Microsoft.Owin;
+using Ninject;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi.OwinHost;
 using Owin;
+using Photogaleries.Data;
 
 [assembly: OwinStartup(typeof(Photogaleries.Services.Startup))]
-
 namespace Photogaleries.Services
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            this.ConfigureAuth(app);
+            app.UseNinjectMiddleware(CreateKernel).UseNinjectWebApi(GlobalConfiguration.Configuration);
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            RegisterMappings(kernel);
+            return kernel;
+        }
+
+        private static void RegisterMappings(StandardKernel kernel)
+        {
+            kernel.Bind<IPhotogaleriesData>()
+                  .To<IPhotogaleriesData>()
+                  .WithConstructorArgument("context",
+                       c => new PhotogaleriesData());
         }
     }
 }
