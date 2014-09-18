c183e4e9a6a7c61eb515fed46f711f79b9f97791
theexar <easlavov@gmail.com>
2014-09-18 12:52:56 +0300

    Added web client skeleton: js libraries, styles, index.html, js entry point. Paths to libraries have been added to require config.

---
+++ Photogaleries.WebClient/app.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="scripts/_references.js" />
+(function () {
+    require.config({
+        paths: {
+            jquery: 'libs/jquery-2.1.1.min',
+            handlebars: 'libs/handlebars-v2.0.0',
+            q: 'libs/q',
+            sammy: 'libs/sammy-latest.min',
+            underscore: 'libs/underscore-min'
+        }
+    });
+
+    require(['underscore', 'handlebars', 'sammy'], function () {
+        console.log('Successful setup!');
+    });
+})()
