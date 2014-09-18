f46e5af0a03ee9423d6c24b014668f114572ffd1
Vali0 <valio_ch@mail.bg>
2014-09-18 13:11:51 +0300

    architecture of js web client

---
+++ Photogaleries.WebClient/scripts/models/http-requester.js
@@ -0,0 +1,35 @@
+﻿define(function () {
+    var httpRequester = (function () {
+        var makeHttpRequest = function (url, type, data) {
+            var deferred = $.Deferred();
+
+            $.ajax({
+                url: url,
+                type: type,
+                data: data,
+                success: function (resultData) {
+                    deferred.resolve(resultData);
+                },
+                error: function (errorMsg) {
+                    deferred.reject(errorMsg);
+                }
+            })
+
+            return deferred.promise();
+        }
+
+        var getJSON = function (url) {
+            return makeHttpRequest(url, 'GET');
+        };
+        var postJSON = function (url, data) {
+            return makeHttpRequest(url, 'POST', data);
+        };
+
+        return {
+            get: getJSON,
+            post: postJSON
+        }
+    }());
+
+    return httpRequester;
+})
